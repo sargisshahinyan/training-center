ff5629b019018efbf76c7b6c5b47f93d9decc6d4
Sargis <shahinyan.sargis@gmail.com>
2019-01-14 12:39:04 +0400

    Fixed npm security issues

---
+++ front/build/precache-manifest.aab931fd93a4970ed2ad80e414338940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e91eae77fcb150f8fa5c",
+    "url": "/static/css/main.4fec951b.chunk.css"
+  },
+  {
+    "revision": "e91eae77fcb150f8fa5c",
+    "url": "/static/js/main.e91eae77.chunk.js"
+  },
+  {
+    "revision": "a983affd2b56aea933e3",
+    "url": "/static/css/1.50dfc951.chunk.css"
+  },
+  {
+    "revision": "a983affd2b56aea933e3",
+    "url": "/static/js/1.a983affd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f40839293bd44a33a3702a7d177e8bbf",
+    "url": "/index.html"
+  }
+];
